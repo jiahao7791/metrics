1b0743e1da9d2b259869d83d98edf41079e3dfa3
Wu Jiahao <wujiahao00@gmail.com>
2022-01-25 00:14:15 +0800

    pls work

--- src/shared/courses.js
+++ src/shared/courses.js
@@ -3,7 +3,7 @@ export const COURSES =
         {
         id: 0,
         name:'Invsble Friends',
-        image: '/assets/images/invisible.jpg',
+        image: 'assets/images/invisible.jpg',
         price:'Mint Price: TBA \n\n Mint Date: 2022-02-01',
         featured: true,
         description:'Discord Server: 58,328 members \n\n Twitter: 3127 Mentions / 10,982 Retweets' 
@@ -11,7 +11,7 @@ export const COURSES =
         {
         id: 1,
         name:'Botborgs',
-        image: '/assets/images/botborgs.jpg',
+        image: 'assets/images/botborgs.jpg',
         price:'Mint Price: TBA \n\n Mint Date: 2022-01-24',
         featured: true,
         description:'Discord Server: 30,309 members \n\n Twitter: 1507 Mentions / 5,182 Retweets' 
@@ -19,7 +19,7 @@ export const COURSES =
         {
         id: 2,
         name:'Lunaroid',
-        image: '/assets/images/lunaroid.jpg',
+        image: 'assets/images/lunaroid.jpg',
         price:'Mint Price: 2 SOL \n\n Mint Date: 2022-01-25',
         featured: true,
         description:'Discord Server: 5,380 members \n\n Twitter: 323 Mentions / 982 Retweets'
